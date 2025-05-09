eaf0353cadc7781a328a58b83a36646f2b4b8bce
Andrej Petreski <petreskia@hotmail.com>
2025-05-09 14:20:32 +0200

    resolved the paths again

--- esolve-media/src/app/layout.tsx
+++ esolve-media/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- esolve-media/src/app/page.tsx
+++ esolve-media/src/app/page.tsx
@@ -1,8 +1,8 @@
-import AboutUs from "@/components/AboutUs";
-import Banner from "@/components/Banner";
-import MarketingForBusinesses from "@/components/MarketingForBusinesses";
-import Navbar from "@/components/Navbar";
-import PersonalBranding from "@/components/PersonalBranding";
+import AboutUs from "../components/AboutUs";
+import Banner from "../components/Banner";
+import MarketingForBusinesses from "../components/MarketingForBusinesses";
+import Navbar from "../components/Navbar";
+import PersonalBranding from "../components/PersonalBranding";
 
 export default function Home() {
   return (
